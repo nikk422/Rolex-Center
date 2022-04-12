b02a94f2d1c85c466e5a8c5adae5f109d0d6ddf9
Nikhil Malviya <94277199+nikk422@users.noreply.github.com>
2022-04-12 23:11:28 +0530

    some minor changes

--- src/routes/authentication-page/login.jsx
+++ src/routes/authentication-page/login.jsx
@@ -3,28 +3,28 @@ import {Link} from "react-router-dom";
 
 export default function Login(){
     return(
-        <div class="container">
-        <div class="login">
+        <div className="container">
+        <div className="login">
             <h1><strong> Login </strong></h1>
             <label for="email">
                 Email address
-                <input type="text" placeholder="abcd@gmail.com" class="email authLable"></input>
+                <input type="text" placeholder="abcd@gmail.com" className="email authLable"></input>
             </label>
             <label for="password">
                 Password
-                <input type="password" placeholder="*********" class="password authLable"></input>
+                <input type="password" placeholder="*********" className="password authLable"></input>
             </label>
             <label for="remember">
-                <input type="checkbox" name="remember" class="rememberBtn"></input>
+                <input type="checkbox" name="remember" className="rememberBtn"></input>
                 Remember me
-                <a class="forget-link" href="/">Forgot your password?</a>
+                <a className="forget-link" href="/">Forgot your password?</a>
             </label>
             <Link to="/Product">
-            <button class="loginBtn">Login</button>
+            <button className="loginBtn">Login</button>
             </Link>
 
             <Link to="/SignUp">
-            <button class="CreateBtn">Create New Acount </button>
+            <button className="CreateBtn">Create New Acount </button>
             </Link>
         </div>
     </div>
